9902bf8f7430fd47b3067571806848804d24323a
convivial-parser-7775 <avelonpang@gmail.com>
2020-10-07 03:07:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def list_dwarves(array)
 	puts "#{index}. * #{element}/"
 end
 
-#end
+end
  #expect(output).to match(/1.*Dopey/)
  #expect(output).to match(/2.*Grumpy/)
  #expect(output).to match(/3.*Bashful/)
